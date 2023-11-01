3ec433e0bfd8518c4d26bed912c73f44dd4029cc
JeanFDev <jeanjfra1@gmail.com>
2023-11-01 13:16:14 -0300

    project created

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TesteComponentComponent } from './components/teste.component/teste.component.component';
+import { IndexComponent } from './pages/index/index.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TesteComponentComponent,
+    IndexComponent,
+    HomeComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
