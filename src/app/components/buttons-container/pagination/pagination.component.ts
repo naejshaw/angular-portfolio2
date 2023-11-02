89b73fd1ac4352e2c1725b80cb4cb76cc33dcc20
JeanFDev <jeanjfra1@gmail.com>
2023-11-02 15:39:26 -0300

    update

--- src/app/components/buttons-container/pagination/pagination.component.ts
+++ src/app/components/buttons-container/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,66 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
-  numberOfItems: number;
-  limitPerPage: number;
-  totalPages!: number;
-  paginationSize: number;
-  currentPage: number;
 
-  constructor() {
-    this.numberOfItems = 0;
-    this.limitPerPage = 3;
-    this.paginationSize = 7;
-    this.currentPage = 1;
-  }
+  @Input()
+  pageId:string =""
+  constructor() { }
 
   ngOnInit(): void {
-    this.numberOfItems = document.querySelectorAll(".card-content .card").length;
-    this.totalPages = Math.ceil(this.numberOfItems / this.limitPerPage);
-
-    this.showPage(1);
   }
 
-  getPageList(totalPages: number, page: number, maxLength: number): number[] {
-    function range(start: number, end: number): number[] {
-      return Array.from(Array(end - start + 1), (_, i) => i + start);
-    }
-
-    const sideWidth = maxLength < 9 ? 1 : 2;
-    const leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
-    const rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
-
-    if (totalPages <= maxLength) {
-      return range(1, totalPages);
-    }
-
-    if (page <= maxLength - sideWidth - 1 - rightWidth) {
-      return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
-    }
-
-    if (page >= totalPages - sideWidth - 1 - rightWidth) {
-      return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
-    }
-
-    return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
-  }
-
-  showPage(whichPage: number): void {
-    if (whichPage < 1 || whichPage > this.totalPages) return;
-
-    this.currentPage = whichPage;
-
-    //TODO: Atualizar a lista de páginas.
-    const pages = this.getPageList(this.totalPages, this.currentPage, this.paginationSize);
-
-    //TODO: Adicionar o código para mostrar as páginas.
-  }
-
-  onNextPageClick(): void {
-    this.showPage(this.currentPage + 1);
-  }
-
-  onPreviousPageClick(): void {
-    this.showPage(this.currentPage - 1);
-  }
 }
--- src/app/components/cards-container/cards-container.component.ts
+++ src/app/components/cards-container/cards-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards-container',
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards-container.component.css']
 })
 export class CardsContainerComponent implements OnInit {
+
+  @Input()
+  itemLeft:string =""
+  @Input()
+  itemMiddle:string =""
+  @Input()
+  itemRight:string =""
+  @Input()
+  cardLeftContent:string =""
+  @Input()
+  cardMiddleContent:string =""
+  @Input()
+  cardRightContent:string =""
 
   constructor() { }
 
