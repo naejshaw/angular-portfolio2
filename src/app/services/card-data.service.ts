880b6a5a211a83c5aff260c463ea813eeef00594
JeanFDev <jeanjfra1@gmail.com>
2023-11-02 22:13:17 -0300

    images links fixed

--- src/app/services/card-data.service.ts
+++ src/app/services/card-data.service.ts
@@ -9,7 +9,7 @@ export class CardDataService {
   constructor() {
     for (let i = 1; i <= 30; i++) {
       this.cardsData.push({
-        cardImageId: `https://github.com/naejshaw/angular-portfolio2/blob/main/src/assets/images/${i}.jpg?raw=true`,
+        cardImageId: `https://github.com/naejshaw/angular-portfolio2/blob/main/src/app/assets/images/${i}.jpg?raw=true`,
         cardTitle: `Card ${i}`,
         cardText: `This is a brief text for Card ${i}.`
       });
